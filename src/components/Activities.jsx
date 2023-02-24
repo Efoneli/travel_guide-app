45c5aaa9f4605b00d5b791e8ad63c28145fd5734
Efoneli <abbaefoneli@gmail.com>
2023-02-24 21:15:07 +0100

    act

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Activities = () => {
   return (
-    <div>
-        <div>
-            <h3>Resorts</h3>
-            <img src="images/resort.jpg" alt="" />
+    <div className='max-w-[1140px] m-auto w-full md:flex mt-[-75px]'>
+        <div className='relative p-4'>
+            <h3 className='abosolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+            <img src="images/resort.jpg" alt="/" />
         </div>
-        <div>
-            <h3>Cruise</h3>
-            <img src="images/cruise.jpg" alt="" />
+        <div className='relative p-4 '>
+            <h3 className='abosolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Cruise</h3>
+            <img src="images/cruise.jpg" alt="/" />
         </div>
-        <div>
-            <h3>Resorts</h3>
-            <img src="images/diving.jpg" alt="" />
+        <div className='relative p-4 '>
+            <h3 className='abosolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+            <img src="images/diving.jpg" alt="/" />
         </div>
     </div>
   )
