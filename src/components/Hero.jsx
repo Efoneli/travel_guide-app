c5bc7e3d2b517e196250f22d3e570cf79d5ff6c1
Efoneli <abbaefoneli@gmail.com>
2023-02-24 19:44:35 +0100

    images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        
+        <img src="images/wil.jpg" alt="mic" />
+        <div>
+            <div>
+                <h1>Find your Special Trip</h1>
+                <h2>With Workaway</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus nesciunt eveniet odit. Alias provident sunt tempore debitis harum ipsam dolores doloribus voluptatum commodi quisquam numquam explicabo dolorum fugiat sequi consectetur recusandae optio deleniti obcaecati aliquam odit, maiores amet porro corrupti! Perferendis voluptates totam cumque esse repellat eos ex accusamus itaque.
+                </p>
+            </div>
+        </div>
     </div>
   )
 }
