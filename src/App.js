26698b0aba00b03563b4a8e5474deeada7175db5
Efoneli <abbaefoneli@gmail.com>
2023-02-24 23:18:13 +0100

    booking comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Activities from './components/Activities';
+import Booking from './components/Booking';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import TopBar from './components/TopBar';
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Activities />
+      <Booking />
     </div>
   );
 }
---
+++ src/components/Booking.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Booking = () => {
+  return (
+    <div id='deals'>
+        <form>
+            <div>
+                <label>Destination</label>
+                <select>
+                    <option>Grande AntLigua</option>
+                    <option>Key West</option>
+                    <option>Maldives</option>
+                    <option>Corumel</option>
+                </select>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Booking
