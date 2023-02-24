5e8b0166c583d53076bc1a7981fbbb65332ee145
Efoneli <abbaefoneli@gmail.com>
2023-02-24 17:43:18 +0100

    nav

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,10 +4,10 @@ import {BsChatSquareDots} from 'react-icons/bs'
 
 const TopBar = () => {
   return (
-    <div className='flex justify-between items-center px-4 py-2'>
+    <div className='bg-red-600 flex justify-between items-center px-4 py-2'>
         <div className='flex items-center'>
             <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2' />
-            <h1 className='text-xl font-bold text-gray-300'>WEEKDAY</h1>
+            <h1 className='text-xl font-bold text-gray-700'>WEEKDAY</h1>
         </div>
         <div className='flex'>
             <div className='hidden md:flex items-center px-6'>
