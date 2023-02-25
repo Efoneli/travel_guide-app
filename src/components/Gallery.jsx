cb9ac9f719dc9dcee1092ce893c7e01801e77a72
Efoneli <abbaefoneli@gmail.com>
2023-02-25 20:56:43 +0100

    gallery done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Gallery = () => {
   return (
-    <div id='gallery'>
-        <h2>Gallery</h2>
-        <div>
-            <div>
-                <img src="images/ahmed.jpg" alt="" />
+    <div id='gallery' className='max-w-[1140px] m-auto w-full px-4 py-16'>
+        <h2 className='text-center tect-gray-700 p-4'>Gallery</h2>
+        <div className='grid sm:grid-cols-5 gap-4 '>
+            <div className='sm:col-span-3 col-span-2 row-span-2'>
+                <img src="images/ahmed.jpg" alt="" className='w-full h-full object-cover ' />
             </div>
             <div>
-                <img src="images/an.jpg" alt="" />
+                <img src="images/an.jpg" alt="" className='w-full h-full object-cover '/>
             </div>
             <div>
-                <img src="images/gerado.jpg" alt="" />
+                <img src="images/gerardo.jpg" alt="" className='w-full h-full object-cover ' />
             </div>
             <div>
-                <img src="images/paolo.jpg" alt="" />
+                <img src="images/paolo.jpg" alt="" className='w-full h-full object-cover '/>
             </div>
             <div>
-                <img src="images/cris.jpg" alt="" />
+                <img src="images/cris.jpg" alt="" className='w-full h-full ' />
             </div>
         </div>
     </div>
