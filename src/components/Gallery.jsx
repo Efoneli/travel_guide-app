7e9956496de337a143547e08c6199611e2256a46
Efoneli <abbaefoneli@gmail.com>
2023-02-25 16:01:58 +0100

    gallery comp

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div id='gallery'>
+        <h2>Gallery</h2>
+        <div>
+            <div>
+                <img src="images/cris.jpg" alt="" />
+            </div>
+            <div>
+                <img src="images/cris.jpg" alt="" />
+            </div>
+            <div>
+                <img src="images/cris.jpg" alt="" />
+            </div>
+            <div>
+                <img src="images/cris.jpg" alt="" />
+            </div>
+            <div>
+                <img src="images/cris.jpg" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Gallery
